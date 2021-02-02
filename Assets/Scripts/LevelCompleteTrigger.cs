874a7eb4ca935d0eaed102ec2a730ca313757a64
MustachedNinja <kostya.yatsuk9@live.com>
2021-02-02 00:33:07 -0800

    Add LevelCompleteTrigger Add LevelCompleteUI Add TextMeshPro Add minimal animation to LevelCompleteUI

---
+++ Assets/Scripts/LevelCompleteTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LevelCompleteTrigger : MonoBehaviour
+{
+
+    [SerializeField] GameObject levelCompleteUI;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            CompleteLevel();
+        }
+    }
+
+    private void CompleteLevel() {
+        Debug.Log("LEVEL COMPLETE");
+        levelCompleteUI.SetActive(true);
+    }
+}
